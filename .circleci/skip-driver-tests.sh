fee3bf59278568207598b47f8684d9c6f22bb232
Cam Saul <github@camsaul.com>
2020-10-19 17:09:35 -0700

    PR feedback

--- .circleci/skip-driver-tests.sh
+++ .circleci/skip-driver-tests.sh
@@ -8,34 +8,33 @@ set -euo pipefail
 
 driver="$1"
 
-commit_message=`cat commit.txt`
-
-parent_branch=`git log --decorate --simplify-by-decoration --oneline | grep -v "HEAD" | head -n 1 | sed -r 's/^\w+\s\(origin\/([^,)]+).+$/\1/'`
-files_with_changes=`git diff --name-only "$parent_branch" | grep '.clj'`
-
 if [[ "$CIRCLE_BRANCH" =~ ^master|release-.+$ ]]; then
     is_master_or_release_branch=true;
 else
     is_master_or_release_branch=false;
 fi
 
-if [[ "$COMMIT_MESSAGE" == *"[ci all]"* ]]; then
+commit_message=`cat commit.txt`
+
+if [[ "$commit_message" == *"[ci all]"* ]]; then
     has_ci_drivers_commit_message=true;
-elif [[ "$COMMIT_MESSAGE" == *"[ci drivers]"* ]]; then
+elif [[ "$commit_message" == *"[ci drivers]"* ]]; then
     has_ci_drivers_commit_message=true;
-elif [[ "$COMMIT_MESSAGE" == *"[ci $driver]"* ]]; then
+elif [[ "$commit_message" == *"[ci $driver]"* ]]; then
     has_ci_drivers_commit_message=true;
 else
     has_ci_drivers_commit_message=false;
 fi
 
-if [[ "$COMMIT_MESSAGE" == *"[ci quick]"* ]]; then
+if [[ "$commit_message" == *"[ci quick]"* ]]; then
     has_ci_quick_message=true;
 else
     has_ci_quick_message=false;
 fi
 
-if [ -n "$files_with_changes" ]; then
+backend_files_with_changes=`cat files_with_changes.txt | grep '*.clj'`
+
+if [ -n "$backend_files_with_changes" ]; then
     has_backend_changes=true;
 else
     has_backend_changes=false;
@@ -48,21 +47,26 @@ echo "Has backend changes?" has_backend_changes
 
 # ALWAYS run driver tests for master or release branches.
 if is_master_or_release_branch; then
+    echo "Running driver tests because this is a build on master or a release branch"
     exit 1;
 fi
 
 # ALWAYS run driver tests if the commit includes [ci all], [ci drivers], or [ci <driver>]
 if has_ci_drivers_commit_message; then
+    echo "Running driver tests because commit message includes [ci all], [ci drivers], or [ci $driver]"
     exit 2;
 fi
 
 # If any backend files have changed, run driver tests *unless* the commit includes [ci quick]
 if has_backend_changes; then
     if has_ci_quick_message; then
+        echo "Skipping driver tests because commit message includes [ci quick]"
         exit 0;
     else
+        echo "Running driver tests because there are changes to backend files in this branch"
         exit 3;
     fi
 fi
 
+echo "Skipping driver tests because branch includes no backend changes"
 exit 0
